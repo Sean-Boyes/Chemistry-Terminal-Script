9f49de628726655b75fe6f880b3078d01de98557
Sean-Boyes <118757050+Sean-Boyes@users.noreply.github.com>
2023-05-20 13:08:38 -0700

    Update Main.py

--- Chem Terminal Script/Main.py
+++ Chem Terminal Script/Main.py
@@ -1,6 +1,34 @@
 # Importing libraries
+from Chem_Numbers import *
 import P_Table
-import Chem_Constants
 import Chem_Conversions
+import Chem_Equations
+        
+# thisIsVariable 
+# this_is_function
 
-# Use library to store known variables, then reset when problem is complete*
+# lists for commands and variables
+
+def message_handeler(userInput):
+    userInput = userInput.split(" ")
+    if userInput.count("=") == 0:
+        # input is command
+        print("is command")
+    else:
+        while userInput.count("=") != 0:
+            # remove all instances of '='
+            userInput.pop(userInput.index("="))
+        # 
+
+# Testing
+
+message = "What variables do you have? \n"
+message += "volume (v) \n"
+message += "moles (n) \n"
+message += "temp (t) \n"
+message += "density (d) \n"
+message += "mass (m) \n"
+message += "pressure (p) \n"
+print(message)
+
+message_handeler(input(""))
