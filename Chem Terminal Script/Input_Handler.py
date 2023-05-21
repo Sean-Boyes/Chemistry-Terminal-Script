97084cf5adc0548e9078dc87a77af64653d753ce
Sean-Boyes <118757050+Sean-Boyes@users.noreply.github.com>
2023-05-20 17:57:38 -0700

    Update Input_Handler.py

--- Chem Terminal Script/Input_Handler.py
+++ Chem Terminal Script/Input_Handler.py
@@ -1,14 +1,20 @@
 # import lib
 from Chem_Numbers import *
+from Chem_Equations import *
 
 def get_input()->str:
     return input(">")
 
 def command_handler(command):
     match command:
-        case 'exit':
-            print("exiting")
+        case 'exit' | 'e':
+            print("exiting...")
             quit()
+        case 'help' | 'h':
+            message = "commands: \n"
+            message += "<exit> or <e> : exits program \n"
+            message += "<help> or <h> : shows command list \n"
+            print(message)
         case _:
             print("Error: Command not Found")
             return
@@ -35,15 +41,18 @@ def message_handeler(userInput):
         # input is command
         command_handler(userInput)
         return
-    
-    userInput = userInput.split(" ")
-    # Clean the input
-    while userInput.count("=") != 0:
-        # remove all instances of '='
-        userInput.pop(userInput.index("="))
-    
-    # There has to be a better way...
-    while len(userInput) > 0:
-        variable_handler(userInput[0],userInput[1])
-        userInput.pop(0)
-        userInput.pop(0)      
+    else:
+        userInput = userInput.split(" ")
+        # Clean the input
+        while userInput.count("=") != 0:
+            # remove all instances of '='
+            userInput.pop(userInput.index("="))
+        
+        # There has to be a better way...
+        while len(userInput) > 0:
+            variable_handler(userInput[0],userInput[1])
+            userInput.pop(0)
+            userInput.pop(0)      
+        
+        get_pressure()
+        print(pressure.value)
